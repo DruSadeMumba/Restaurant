3a1f8c948444435b7d40ad97621a7f43e863cb53
DruSadeMumba <drusademumba@gmail.com>
2022-05-22 17:41:44 +0300

    Add restaurants activity

--- app/src/main/java/com/drusade/myrestaurants/MainActivity.java
+++ app/src/main/java/com/drusade/myrestaurants/MainActivity.java
@@ -1,8 +1,8 @@
 package com.drusade.myrestaurants;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import androidx.appcompat.app.*;
+import android.os.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Ouchie! \uD83D\uDE31", Toast.LENGTH_LONG).show();
+        });
     }
 }
